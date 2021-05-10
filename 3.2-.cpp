958cf758ae3ae69aad13381a6da48c153440a76d
themechanicalguy <themechanicalguy2021@gmail.com>
2021-05-11 00:47:09 +0530

    Create 3.2-.cpp

---
+++ 3.2-.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int countPositiveNumbers(int A[], int n)
+{
+    int pos_count = 0;
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (A[i] > 0)
+            pos_count++;
+    }
+    return pos_count;
+}
+int countNegativeNumbers(int A[], int n)
+{
+    int neg_count = 0;
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        if (A[i] < 0)
+            neg_count++;
+    }
+    return neg_count;
+}
+// Function to print the array
+void printArray(int A[], int n)
+{
+    int i;
+
+    cout << "Array: " << endl;
+    for (i = 0; i < n; i++)
+    {
+        cout << A[i] << " ,";
+    }
+    cout << endl;
+}
+// Driver program
+int main()
+{
+    int A[] = {2, -1, 5, 6, 0, -3};
+    int n;
+    n = sizeof(A) / sizeof(A[0]);
+
+    printArray(A, n);
+
+    cout << "Count of Positive elements " << countPositiveNumbers(A, n) << endl;
+    cout << "Count of Negative elements " << countNegativeNumbers(A, n) << endl;
+
+    return 0;
+}
