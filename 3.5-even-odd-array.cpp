af40704d9e2831e14e0517569d375b400c3e134f
themechanicalguy <themechanicalguy2021@gmail.com>
2021-05-12 18:26:40 +0530

    Code Updated

--- 3.5-even-odd-array.cpp
+++ 3.5-even-odd-array.cpp
@@ -1,55 +1,43 @@
-// Program to seperate even and odd in an array.
-// C++ program to segregate even and odd elements of array
 #include <iostream>
 using namespace std;
-
-/* Function to swap *a and *b */
 void swap(int *a, int *b);
-
-void segregateEvenOdd(int arr[], int size)
+void segregateEvenOdd(int A[], int n)
 {
-    /* Initialize left and right indexes */
-    int left = 0, right = size - 1;
-    while (left < right)
+    int l = 0, r = n - 1;
+    while (l < r)
     {
-        /* Increment left index while we see 0 at left */
-        while (arr[left] % 2 == 0 && left < right)
-            left++;
-
-        /* Decrement right index while we see 1 at right */
-        while (arr[right] % 2 == 1 && left < right)
-            right--;
-
-        if (left < right)
+        while (A[l] % 2 == 0 && l < r)
         {
-            /* Swap arr[left] and arr[right]*/
-            swap(&arr[left], &arr[right]);
-            left++;
-            right--;
+            l++;
+        }
+        while (A[r] % 2 == 1 && l < r)
+        {
+            r--;
+        }
+        if (l < r)
+        {
+            swap(&A[l], &A[r]);
+            l++;
+            r--;
         }
     }
 }
-
-/* UTILITY FUNCTIONS */
 void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-
-/* Driver code */
 int main()
 {
-    int arr[] = {12, 34, 45, 9, 8, 90, 3};
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    int A[] = {12, 34, 45, 9, 8, 90, 3};
+    int n = sizeof(A) / sizeof(A[0]);
     int i = 0;
-
-    segregateEvenOdd(arr, arr_size);
-
-    cout << "Array after segregation ";
-    for (i = 0; i < arr_size; i++)
-        cout << arr[i] << " ";
-
+    segregateEvenOdd(A, n);
+    cout << "Array after segregation" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i] << " ";
+    }
     return 0;
 }
