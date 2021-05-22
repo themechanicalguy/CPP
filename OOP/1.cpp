5ae9e8adb3c0b91bd782d3810eee372b5ac8f28d
themechanicalguy <themechanicalguy2021@gmail.com>
2021-05-22 11:43:24 +0530

    OOP intro

---
+++ OOP/1.cpp
@@ -0,0 +1,30 @@
+//Calculate area and perimeter of rectrangle
+#include <iostream>
+using namespace std;
+class Reactangle
+{
+public:
+    int length;
+    int breadth;
+
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length * breadth);
+    }
+};
+int main()
+{
+    Reactangle r1, r2;
+    r1.length = 10;
+    r1.breadth = 5;
+    cout << r1.area() << endl;
+    r2.length = 15;
+    r2.breadth = 10;
+    cout << r2.area() << endl;
+
+    return 0;
+}
