5a5e2d5b02037cd0a7bc339b5a19b9077d63c4ec
themechanicalguy <themechanicalguy2021@gmail.com>
2021-05-23 00:32:20 +0530

    Updated m and n to r and c

--- muilti-dimensional-array/1-input-output-2D-array.cpp
+++ muilti-dimensional-array/1-input-output-2D-array.cpp
@@ -3,30 +3,30 @@
 using namespace std;
 int main()
 {
-    int m, n, key;
-    cin >> m >> n;
-    int A[m][n];
-    for (int i = 0; i < m; i++)
+    int r, c, key;
+    cin >> r >> c;
+    int A[r][c];
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < c; j++)
         {
             cin >> A[i][j];
         }
     }
     cout << endl;
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < c; j++)
         {
             cout << A[i][j] << " ";
         }
         cout << endl;
     }
     cout << endl;
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < c; j++)
         {
             cout << i << j << " ";
         }
