c37ccec3078fbeafb93127d515349b2042501726
themechanicalguy <themechanicalguy2021@gmail.com>
2021-05-26 21:47:14 +0530

    Polymorphism updated

---
+++ OOP/9.2-virtual-function2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class BasicCar
+{
+public:
+    virtual void start()
+    {
+        cout << "BasicCar Started" << endl;
+    }
+};
+
+class AdvanceCar : public BasicCar
+{
+public:
+    void start()
+    {
+        cout << "AdvanceCar Started" << endl;
+    }
+};
+
+int main()
+{
+    BasicCar *ptr = new AdvanceCar();
+    ptr->start();
+}
