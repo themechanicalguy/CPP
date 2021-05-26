79f229487cce64475d2be069fc5be9fc92ed1eb1
themechanicalguy <themechanicalguy2021@gmail.com>
2021-05-26 18:03:19 +0530

    updated

--- OOP/8-base-class-pointer-derived-cls-p.cpp
+++ OOP/8-base-class-pointer-derived-cls-p.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 using namespace std;
 
-/*program for demo#1-base class pointer derived class object
+class BasicCar
+{
+public:
+    void start()
+    {
+        cout << "Car Started" << endl;
+    }
+};
 
-*/
-class base
+class AdvanceCar : public BasicCar
 {
 public:
-    void fun1()
+    void playmusic()
     {
-        cout << "fun1 of base" << endl;
+        cout << "Playing Music" << endl;
     }
 };
-class derived : public base
-{
-public:
-    void fun1()
-    {
-        cout << "fun2 of derived" << endl;
-    }
-};
+
 int main()
 {
-    derived d;
-    base *p = &d;
-    ptr->fun1();
-    ptr->fun2();
-    return 0;
+    BasicCar b;
+    AdvanceCar *q = &b;
+    AdvanceCar a;
+    a.start();
+    a.playmusic();
+    BasicCar *ptr = &a;
+    ptr->start();
+    ptr->playmusic(); // this function cannot be called.
 }
